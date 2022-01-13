7a8c2d065dd49e6cc3b1485bbc6d63247660936d
benjamindean <ben@thefoghill.com>
2022-01-13 14:50:20 -0500

    commit tree 191cae6474f4eeae28c459a4a41aa2bd6a1c0762

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = () => 2000/100
+
+const add = (parameter1, parameter2) => {
+    return parameter1 + parameter2;
+  }
+add(10,10);
+
+const square = (parameter1) => {;
+    return parameter1*parameter1
+  }
+square();
